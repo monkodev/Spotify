35c81fd50a4b8b762133506b2e1f932397aa800f
MonKoDev Technologies <gmalugani@gmail.com>
2017-07-12 15:01:49 -0300

    Implementación final

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
 var showTracks = function() {
+
 	var template = Handlebars.compile($('#track-template').html());
-	var accessToken = "BQBJ2GuWvEHCBDxoZHU2rQu5YyhW545h2ZGLn6v3-m-6DjkPCtMLa24b480FiVQAnAdL6GjnAGVwW7wmAiecV0ykvVk1m0lIoE9Ocb-HUJ8UE7nleZWwZQtxgWvZt7qL8S_-zEkm";
-	var refresh_token='';
-	var client_id= "8e8f737bf9354f81a69f0d43724a9c64";
-	var client_secret= "92772bee217f40b98a5db9cdea602a0e";
+	var accessToken = $('#intoken').val();
+
 $.ajax({
-    url: 'https://api.spotify.com/v1/search?q=Muse&type=track',
+    url: 'https://api.spotify.com/v1/search?q=' + $('#in').val()+'&type=track',
     dataType: "json",
     type: 'GET',
     headers: {
        'Authorization': 'Bearer ' + accessToken 
    },
    success: function (data){
-        $('#resultados').replaceWith(template({ tracks: data }));
+        $('#resultados2').replaceWith(template({ tracks: data }));
         console.log(data);
         
     },
@@ -20,21 +19,24 @@ $.ajax({
 
 };
 
-var get_refreshtokens = function() {
-
-
-	$.ajax({
-		url: 'https://accounts.spotify.com/api/token',
-		dataType: "json",
-    	type: 'POST',
-	});
-};
-
-
-
 $('#in').keypress(function(e) {
-   if(e.which == 13) {
+	
+   if(e.which == 13) {	
+   		
         showTracks();
+        
         $('#in').val('');
+        $('#intoken').val('');
+    }    
+});
+
+$('#intoken').keypress(function(e) {
+	
+   if(e.which == 13) {	
+   		
+        showTracks();
+        
+        $('#in').val('');
+        $('#intoken').val('');
     }    
 });
